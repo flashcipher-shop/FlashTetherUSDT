d30e383fe768337a20dbfb5fa8eda715bfde8ab1
LikhonX <likhoncryptocalls@gmail.com>
2024-08-14 00:25:43 +0000

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { AlertCircle, DollarSign, Send, ExternalLink, Users, Zap } from 'lucide-react';
+const USDTContractAddress = "0x548c937b3df98b2309334f7485e02ab0affbb00d";
+const USDT_ABI = []; // Placeholder for ABI
 
-const USDTContractAddress = "0x548c937b3df98b2309334f7485e02ab0affbb00d"; // Mainnet address of USDT
-const USDT_ABI = [{ "constant": true, "inputs": [], "name": "name", "outputs": [{ "name": "", "type": "string" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_upgradedAddress", "type": "address" }], "name": "deprecate", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "approve", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "deprecated", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_evilUser", "type": "address" }], "name": "addBlackList", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_from", "type": "address" }, { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "transferFrom", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "upgradedAddress", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "address" }], "name": "balances", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "maximumFee", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "_totalSupply", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [], "name": "unpause", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "_maker", "type": "address" }], "name": "getBlackListStatus", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "address" }, { "name": "", "type": "address" }], "name": "allowed", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "paused", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "who", "type": "address" }], "name": "balanceOf", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [], "name": "pause", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "getOwner", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "owner", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [{ "name": "", "type": "string" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "transfer", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "newBasisPoints", "type": "uint256" }, { "name": "newMaxFee", "type": "uint256" }], "name": "setParams", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "amount", "type": "uint256" }], "name": "issue", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "amount", "type": "uint256" }], "name": "redeem", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "_owner", "type": "address" }, { "name": "_spender", "type": "address" }], "name": "allowance", "outputs": [{ "name": "remaining", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "basisPointsRate", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "address" }], "name": "isBlackListed", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_clearedUser", "type": "address" }], "name": "removeBlackList", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "MAX_UINT", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "newOwner", "type": "address" }], "name": "transferOwnership", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "_blackListedUser", "type": "address" }], "name": "destroyBlackFunds", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "name": "_initialSupply", "type": "uint256" }, { "name": "_name", "type": "string" }, { "name": "_symbol", "type": "string" }, { "name": "_decimals", "type": "uint256" }], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "amount", "type": "uint256" }], "name": "Issue", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "amount", "type": "uint256" }], "name": "Redeem", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "newAddress", "type": "address" }], "name": "Deprecate", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "feeBasisPoints", "type": "uint256" }, { "indexed": false, "name": "maxFee", "type": "uint256" }], "name": "Params", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "_blackListedUser", "type": "address" }, { "indexed": false, "name": "_balance", "type": "uint256" }], "name": "DestroyedBlackFunds", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "_user", "type": "address" }], "name": "AddedBlackList", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "_user", "type": "address" }], "name": "RemovedBlackList", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "owner", "type": "address" }, { "indexed": true, "name": "spender", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" }], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "from", "type": "address" }, { "indexed": true, "name": "to", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" }], "name": "Transfer", "type": "event" }, { "anonymous": false, "inputs": [], "name": "Pause", "type": "event" }, { "anonymous": false, "inputs": [], "name": "Unpause", "type": "event" }];
+const generateData = () => [...Array(7)].map((_, i) => ({
+  day: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'][i],
+  amount: Math.floor(Math.random() * 1000) + 100
+}));
 
-function App() {
+const AdCard = ({ title, description, link, icon }) => (
+  <div className="mb-4 overflow-hidden rounded-lg shadow-lg transition-transform duration-300 ease-in-out hover:scale-105">
+    <div className="bg-gradient-to-r from-purple-600 to-indigo-600 p-4">
+      <div className="flex items-center justify-between">
+        <h3 className="text-xl font-bold text-white">{title}</h3>
+        {React.createElement(icon, { className: "h-8 w-8 text-white" })}
+      </div>
+    </div>
+    <div className="p-4 bg-white">
+      <p className="mb-4 text-gray-600">{description}</p>
+      <a 
+        href={link} 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        className="inline-flex items-center text-indigo-600 hover:text-indigo-800 transition-colors duration-200"
+      >
+        Explore <ExternalLink className="ml-1 h-4 w-4" />
+      </a>
+    </div>
+  </div>
+);
+
+const App = () => {
   const [recipient, setRecipient] = useState('');
   const [amount, setAmount] = useState('');
   const [transactionHash, setTransactionHash] = useState('');
   const [loading, setLoading] = useState(false);
+  const [chartData, setChartData] = useState(generateData());
+  const [onlineUsers, setOnlineUsers] = useState(0);
+  const [totalSends, setTotalSends] = useState(0);
+  const [progress, setProgress] = useState(0);
+
+
+  useEffect(() => {
+    if (window.Telegram?.WebApp) {
+      window.Telegram.WebApp.ready();
+      window.Telegram.WebApp.MainButton.setText('Send USDT').show();
+      window.Telegram.WebApp.onEvent('mainButtonClicked', sendUSDT);
+    }
+
+    // Simulate fetching online users
+    const interval = setInterval(() => {
+      setOnlineUsers(Math.floor(Math.random() * 100) + 50);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   const sendUSDT = async () => {
     const privateKey = "c59b7a9001cea7f96e82a487bc5c92994ee70907a55d3adcd820991fe8b9cf4e"; // Replace with your actual private key
@@ -17,43 +65,139 @@ function App() {
     const usdtContract = new ethers.Contract(USDTContractAddress, USDT_ABI, wallet);
 
     setLoading(true);
-
+    setProgress(0);
     try {
-      const tx = await usdtContract.transfer(recipient, ethers.utils.parseUnits(amount, 6)); // USDT has 6 decimals
-      setTransactionHash(tx.hash);
-      console.log('Transaction Hash:', tx.hash);
-      await tx.wait(); // Wait for the transaction to be confirmed
-      console.log('Transaction Confirmed');
-      alert('Transaction Confirmed');
-      setLoading(false);
-    } catch (error) {
-      console.error('Error sending USDT:', error);
-    }
-  };
+      // Simulating transaction steps
+      for (let i = 0; i < 5; i++) {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        setProgress((i + 1) * 20);
+      }
 
+      const provider = new ethers.providers.JsonRpcProvider('https://eth-sepolia.g.alchemy.com/v2/mMVePgCu6ccl4ROMHh892qE0nNYOP4_G');
+      const signer = provider.getSigner();
+      const usdtContract = new ethers.Contract(USDTContractAddress, USDT_ABI, signer);
+ 
+    const tx = await usdtContract.transfer(recipient, ethers.utils.parseUnits(amount, 6)); // USDT has 6 decimals
+    setTransactionHash(tx.hash);
+    console.log('Transaction Hash:', tx.hash);
+    await tx.wait(); // Wait for the transaction to be confirmed
+    console.log('Transaction Confirmed');
+    alert('Transaction Confirmed');
+    setLoading(false);
+  } catch (error) {
+    console.error('Error sending USDT:', error);
+    setLoading(false);
+    setProgress(100);
+  }
+};
   return (
-    <div className="bg-gradient-to-tr from-fuchsia-300 to-sky-500">
-      <section id="login" className="p-4 flex flex-col justify-center min-h-screen max-w-md mx-auto">
-        <div className="p-6 bg-sky-100 rounded">
-          <div className="flex items-center justify-center font-black m-3 mb-12">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 mr-3 text-red-600 animate-pulse" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
-            </svg>
-            <h1 className="tracking-wide text-3xl text-gray-900">Flash USDT Sender</h1>
+    <div className="bg-gradient-to-br from-gray-900 to-gray-800 min-h-screen p-4 text-white">
+      <div className="max-w-6xl mx-auto">
+
+        
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <div className="bg-gray-800 border-gray-700 p-6 rounded-xl shadow-xl animate-slide-in-left">
+            <h2 className="text-2xl font-bold text-white mb-4">Send USDT</h2>
+            <div className="space-y-4">
+              <input
+                value={recipient}
+                onChange={(e) => setRecipient(e.target.value)}
+                placeholder="Recipient Address"
+                className="w-full bg-gray-700 border-gray-600 text-white placeholder-gray-400 p-3 rounded-md"
+              />
+              <input
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+                placeholder="Amount"
+                type="number"
+                className="w-full bg-gray-700 border-gray-600 text-white placeholder-gray-400 p-3 rounded-md"
+              />
+              <button
+                onClick={sendUSDT}
+                disabled={loading}
+                className="w-full bg-gradient-to-r from-purple-500 to-indigo-600 hover:from-purple-600 hover:to-indigo-700 text-white font-bold py-3 px-4 rounded-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105"
+              >
+                {loading ? 'Sending...' : 'Send USDT'}
+                <Send className="ml-2 h-5 w-5 inline" />
+              </button>
+              {loading && (
+                <Progress value={progress} className="w-full" />
+              )}
+              {transactionHash && (
+                <p className="text-sm text-gray-400">
+                  Transaction Hash: {transactionHash}
+                </p>
+              )}
+            </div>
           </div>
-          <div className="flex flex-col justify-center p-2">
-            <label className="text-lg font-medium">Receipt Wallet Address :</label>
-            <input value={recipient} onChange={e => setRecipient(e.target.value)} className="mb-3 px-2 py-1.5 mt-1 block w-full border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 focus:invalid:border-red-500 focus:invalid:ring-red-500" type="text" placeholder="0x12345678901234567890" />
-            <label className="text-lg font-medium">Transfer Amount :</label>
-            <input value={amount} onChange={e => setAmount(e.target.value)} className="mb-3 px-2 py-1.5 mt-1 block w-full border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 focus:invalid:border-red-500 focus:invalid:ring-red-500" type="text" placeholder="0" />
-            <button className="px-4 py-1.5 rounded-md shadow-lg bg-gradient-to-r from-pink-600 to-red-600 font-medium text-gray-100 block transition duration-300" onClick={sendUSDT}>
-              <span> {loading ? 'Sending...' : 'Send USDT'}</span>
-            </button>
+
+          <div className="bg-gray-800 border-gray-700 p-6 rounded-xl shadow-xl md:col-span-2 animate-slide-in-right">
+            <h2 className="text-2xl font-bold text-white mb-4">Transaction History</h2>
+            <ResponsiveContainer width="100%" height={300}>
+              <LineChart data={chartData}>
+                <CartesianGrid strokeDasharray="3 3" stroke="#374151" />
+                <XAxis dataKey="day" stroke="#9CA3AF" />
+                <YAxis stroke="#9CA3AF" />
+                <Tooltip 
+                  contentStyle={{ backgroundColor: '#374151', border: 'none' }}
+                  itemStyle={{ color: '#E5E7EB' }}
+                />
+                <Line type="monotone" dataKey="amount" stroke="#8B5CF6" strokeWidth={2} dot={{ fill: '#8B5CF6' }} />
+              </LineChart>
+            </ResponsiveContainer>
           </div>
         </div>
-      </section>
-    </div>
-  )
-}
 
-export default App
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-3 gap-6 animate-fade-in">
+          <div className="bg-gray-800 border-gray-700 p-6 rounded-xl shadow-xl flex items-center justify-between">
+            <div>
+              <h3 className="text-xl font-bold text-white">Online Users</h3>
+              <p className="text-3xl font-bold text-purple-400">{onlineUsers}</p>
+            </div>
+            <Users className="h-12 w-12 text-purple-400" />
+          </div>
+          <div className="bg-gray-800 border-gray-700 p-6 rounded-xl shadow-xl flex items-center justify-between">
+            <div>
+              <h3 className="text-xl font-bold text-white">Total Sends</h3>
+              <p className="text-3xl font-bold text-indigo-400">{totalSends}</p>
+            </div>
+            <Zap className="h-12 w-12 text-indigo-400" />
+          </div>
+          <div className="bg-gray-800 border-gray-700 p-6 rounded-xl shadow-xl">
+            <h3 className="text-xl font-bold text-white mb-2">Quick Stats</h3>
+            <p className="text-gray-400">24h Volume: 1,234,567 USDT</p>
+            <p className="text-gray-400">Avg. Transaction: 5,678 USDT</p>
+          </div>
+        </div>
+
+        <div className="mt-12 animate-fade-in">
+          <h2 className="text-3xl font-bold mb-6 text-center text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+            Featured Apps
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <AdCard
+              title="Crypto Tools Pro"
+              description="Advanced crypto trading and analysis tools to supercharge your strategy."
+              link="https://scripters.shop/"
+              icon={DollarSign}
+            />
+            <AdCard
+              title="FlashCipher Guard"
+              description="Next-gen security solutions to fortify your crypto assets."
+              link="https://flashcipher.shop/"
+              icon={AlertCircle}
+            />
+          </div>
+        </div>
+
+        <div className="mt-12 text-center text-sm text-gray-400 animate-fade-in">
+          <p>
+            Always verify transaction details before sending. For support, contact our team.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
